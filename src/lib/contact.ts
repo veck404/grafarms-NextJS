fc5c68c531c9b90e01e00697d203dc7f76771308
Veck404 <victor7ultimate@gmail.com>
2025-11-13 14:36:16 +0100

    Deuteronomy

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = "2347017567105";
+export const WHATSAPP_NUMBER = "2348055112729";
 
 export const buildWhatsAppCheckoutLink = (message: string) =>
   `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
